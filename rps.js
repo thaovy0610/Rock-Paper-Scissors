4978cad8de9fe9fa8daefe9400b94d6367179f7a
thaovy0610 <thaovy0610@gmail.com>
2024-08-06 13:03:38 +0700

    Add show result function

--- rps.js
+++ rps.js
@@ -5,6 +5,9 @@ const scissors = document.querySelector("#scissors")
 const playerResult = document.querySelector("#humanscore")
 const computerResult = document.querySelector("#computerscore")
 const result = document.createElement("p")
+const winnerMsg = document.createElement("p")
+const optionBtn = document.querySelector("#btn")
+const resetBtn = document.createElement("button")
 
 let playerScore = 0
 let computerScore = 0
@@ -17,11 +20,9 @@ function getComputerChoice() {
 }
 
 function hasPlayerWinRound(player, computer) {
-    if (player == "Rock" & computer == "Scissors"
-     || player == "Paper" & computer == "Rock"
-     || player == "Scissors" & computer == "Paper") {
-        return true
-     } else return false
+    return (player == "Rock" && computer == "Scissors"
+     || player == "Paper" && computer == "Rock"
+     || player == "Scissors" && computer == "Paper") 
 }
 
 function getRoundResult(playerChoice) {
@@ -29,17 +30,36 @@ function getRoundResult(playerChoice) {
     if (hasPlayerWinRound(playerChoice, computerChoice)) {
         playerScore++
         playerResult.innerText = playerScore
+        result.textContent = `You win! ${playerChoice} beats ${computerChoice}`
+    } else if (playerChoice == computerChoice) {
+        result.textContent = `Draw, both chose ${playerChoice}`
     } else {
         computerScore++
         computerResult.innerText = computerScore
+        result.textContent = `You lose! ${computerChoice} beats ${playerChoice}`
+    }
+    if (playerScore == 5) {
+        winnerMsg.textContent = "You have won the game !!!!"
+        optionBtn.style.display = 'none'
+        resetBtn.style.display = 'block'
+    }
+    if (computerScore == 5) {
+        winnerMsg.textContent = "You have won the game !!!!"
+        optionBtn.style.display = 'none'
+        resetBtn.style.display = 'block'
     }
 }
 
-function battle() {
-    rock.addEventListener("click", () => {
-        console.log("RUN")
-        getRoundResult("Rock")
-    })
-}
-battle()
+rock.addEventListener("click", () => {
+    getRoundResult("Rock")
+})
+paper.addEventListener("click", () => {
+    getRoundResult("Paper")
+})
+scissors.addEventListener("click", () => {
+    getRoundResult("Scissors")
+})
+
 container.appendChild(result)
+container.appendChild(winnerMsg)
+// container.appendChild(resetBtn)
